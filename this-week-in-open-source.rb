595937d9c34d5e77e687eb51574fb9e351d121c1
Sara Jackson <csara@thoughtbot.com>
2023-06-09 08:38:20 -0400

    Tidying things up a little

--- this-week-in-open-source.rb
+++ this-week-in-open-source.rb
@@ -2,21 +2,20 @@
 
 require "github_api"
 
-puts "This Week In Open Source"
-puts ""
+puts "\nThis Week In Open Source\n"
 one_week_in_seconds = 60 * 60 * 24 * 7
 github = Github.new
 owner = ARGV[0]
 repo_list = ARGV[1..]
 repo_list.each do |repo|
-  commits = github.repos.commits.list owner, repo, since: Time.now - one_week_in_seconds
-  commit_hash_by_author = Hash.new([])
-  commits.each { |commit| commit_hash_by_author[commit.author.login] = commit_hash_by_author[commit.author.login] << commit }
-  next unless commits.any?
+  commit_list = github.repos.commits.list owner, repo, since: Time.now - (one_week_in_seconds*2)
+  commit_hash_by_author = commit_list.group_by { |commit| commit.author.login }
+  next unless commit_list.any?
+
   puts "## [#{repo}](#{github.repos.get(owner, repo).html_url}) \n\n"
-  commit_hash_by_author.each do |author, commits|
-    commit = commits.first
-    puts "#{commit.commit.author.name}([#{author}](#{commit.author.html_url}))"
+  commit_hash_by_author.each do |author_handle, commits|
+    first_commit = commits.first
+    puts "#{first_commit.commit.author.name} ([#{author_handle}](#{first_commit.author.html_url}))"
     commits.each do |commit| 
       puts "    [#{commit.sha[0...7]}](#{commit.html_url})"
     end
